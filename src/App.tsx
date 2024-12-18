f6ef0563fd9c318e0eb2de600310e44aa8489eea
Leo en laptop <leonar2zb@gmail.com>
2024-12-18 16:33:54 -0300

    Instalado Headless UI y creado componente para modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react"
 import BudgetForm from "./components/BudgetsForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
+import ExpenseModal from "./components/ExpenseModal"
 
 function App() {
   const { state } = useBudget()
@@ -16,6 +17,11 @@ function App() {
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
         {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   )
 }
