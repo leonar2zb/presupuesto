45c4ba830cc2739d13bfb0c20206d71edb138cc7
Leo en laptop <leonar2zb@gmail.com>
2024-12-18 15:42:42 -0300

    Mostrando componente condicionalmente según validez del presupuesto

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useMemo } from "react"
 import BudgetForm from "./components/BudgetsForm"
+import { useBudget } from "./hooks/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
 
 function App() {
-    
+  const { state } = useBudget()
+  const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
@@ -10,7 +14,7 @@ function App() {
         </h1>
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm />
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
       </div>
     </>
   )
