38c67755daef708a331ffe9b0d2f07d87b9fcd81
Leo en laptop <leonar2zb@gmail.com>
2024-12-26 18:42:24 -0300

    Actualizando presupuesto, disponible y gastado

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
 import BudgetForm from "./components/BudgetsForm"
 import { useBudget } from "./hooks/useBudget"
 import BudgetTracker from "./components/BudgetTracker"
@@ -8,6 +8,12 @@ import ExpenseList from "./components/ExpenseList"
 function App() {
   const { state } = useBudget()
   const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
+
+  useEffect(() => {
+    localStorage.setItem('budget', state.budget.toString())
+    localStorage.setItem('expenses', JSON.stringify(state.expenses))
+  }, [state])
+
   return (
     <>
       <header className="bg-blue-600 py-8 max-h-72">
